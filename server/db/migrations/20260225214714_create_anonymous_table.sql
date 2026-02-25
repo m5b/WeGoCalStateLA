7b4590514bc646a814e1ec7b66f4567c2686de48
Miguel <gonzalez98m@gmail.com>
2026-02-25 14:01:59 -0800

    feat: create migration for anonymous_name table

---
+++ server/db/migrations/20260225214714_create_anonymous_table.sql
@@ -0,0 +1,11 @@
+-- migrate:up
+    CREATE TABLE anonymous_name (
+        user_id INT UNSIGNED NOT NULL,
+        anonymous_name VARCHAR(255) NOT NULL,
+        taken TINYINT(1) NOT NULL DEFAULT 0,
+        CONSTRAINT fk_anonymous_user FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
+
+    );
+
+-- migrate:down
+
