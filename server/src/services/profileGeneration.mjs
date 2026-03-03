6628e18f8e1b62bf25d725d121bcd8ecbdd118c9
Miguel <gonzalez98m@gmail.com>
2026-03-03 15:13:49 -0800

    feat: add anonymous profile repository and profile generation service

---
+++ server/src/services/profileGeneration.mjs
@@ -0,0 +1,14 @@
+
+
+
+export default async function generateAnonymous() {
+
+    let total = //function to get the total of not taken profiles ;
+
+    // while(total < 1000){
+
+    //     //code to make the new profile names;
+
+    // }
+
+}
