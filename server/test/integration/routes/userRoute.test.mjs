6cc79439d850f44527deb242a30c527f61e96880
Just-ARandomGuy <baohang873@gmail.com>
2026-03-03 21:23:50 -0800

    fixed redisURL issue and add await for promises in hook

--- server/test/integration/routes/userRoute.test.mjs
+++ server/test/integration/routes/userRoute.test.mjs
@@ -28,7 +28,7 @@ describe("userRoute Integration", () => {
     })
 
     afterEach(async () => {
-        connection.rollback()
+        await connection.rollback()
         connection.release()
     })
     afterAll(async ()=> {
