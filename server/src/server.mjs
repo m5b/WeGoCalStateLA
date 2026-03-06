a64f30d0a36d1af6269033f42d647535de2b3831
Just-ARandomGuy <baohang873@gmail.com>
2026-03-06 02:12:34 -0800

    remove use of https for better development enviroment

--- server/src/server.mjs
+++ server/src/server.mjs
@@ -1,25 +1,12 @@
 import "./config/loadEnv.mjs"
-import * as https from 'node:https'
-import * as fs from 'node:fs'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
 import { createApp } from './app/app.mjs'
 import connectionPool from "./lib/pool.mjs";
 import {redis} from "./lib/redis.mjs";
 import {createEmailService} from "./services/auth/email/emailService.mjs";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-const keyPath = path.join(__dirname, '../certs/localhost-key.pem')
-const certPath = path.join(__dirname, '../certs/localhost.pem')
 const emailService = createEmailService()
 const app = createApp(connectionPool, redis, emailService)
-const sslOption = {
-    key: fs.readFileSync(keyPath, "utf-8"),
-    cert: fs.readFileSync(certPath, "utf-8")
-}
-
-https.createServer(sslOption, app).listen(
-    process.env.PORT || 3000
-)
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Express server running at http://localhost:${port}/`);
+});
