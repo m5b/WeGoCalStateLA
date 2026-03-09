f7f4e7abb20cde69c314c64368b5294c0c12af33
Just-ARandomGuy <baohang873@gmail.com>
2026-03-09 15:01:26 -0700

    fixed parameter issue

--- server/src/routes/auth/signup.mjs
+++ server/src/routes/auth/signup.mjs
@@ -15,10 +15,8 @@ export function createSignupRouter({signupTokenService, voprfService, userServic
         const emailHash = await voprfService.handleServerVOPRF(email)
         const passwordHash = await passwordService.hashPassword(password)
         //sign up the user
-        const user = await userService.createUser(emailHash, passwordHash)
-        res.json(jsend.success({
-            userUuid: user.userUuid
-        }))
+        const user = await userService.createUser({emailHash, passwordHash})
+        res.json(jsend.success(null))
     })
     return router
 }
