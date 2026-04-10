7848094716c055662d33f0f80c71299c3efe81ae
a2ibanez <a2ibanez2022@gmail.com>
2026-04-09 19:47:24 -0700

    minor changes

--- server/src/routes/anonymous.mjs
+++ server/src/routes/anonymous.mjs
@@ -16,7 +16,7 @@ router.get("/", requireJwtAuth, async (req, res) => {
 
     // retrieve anonymous username
     try{
-        var anon_username = getAnonNameByUserId(userId)
+        var anon_username = await getAnonNameByUserId(userId)
     } catch (err){
         res.status(404).send(jsend.fail({ "reason" : err.message}))
     }
@@ -35,7 +35,7 @@ router.get("/assign", requireJwtAuth, async (req, res) => {
 
     // retrieve anonymous username
     try{
-        var anon_username = getAnonNameByUserId(userId)
+        var anon_username = await getAnonNameByUserId(userId)
     } catch (err){
         res.status(404).send(jsend.fail({ "reason" : err.message}))
     }
