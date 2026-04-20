442a68d33da33a072c0ba4a1a871366ff846cec5
Just-ARandomGuy <baohang873@gmail.com>
2026-04-19 18:58:28 -0700

    move keyclaok provider into different github project for easier organize

--- infra/keycloak/providers/keycloakify-mui-starter/src/login/voprf/hash.ts
+++
@@ -1,8 +0,0 @@
-import { hkdf } from "@noble/hashes/hkdf.js";
-import { sha256 } from "@noble/hashes/sha2.js";
-
-const te = new TextEncoder();
-
-export function hkdfWegoV1(input: Uint8Array) {
-    return hkdf(sha256, input, te.encode("wego-v1"), te.encode("wego-auth"), 32);
-}
