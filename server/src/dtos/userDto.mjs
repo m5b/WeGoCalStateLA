0f08328fc48c0c4f946cacbc32bf0734c84be22b
Just-ARandomGuy <baohang873@gmail.com>
2026-04-19 20:41:20 -0700

    remove the not exist field due to change of db

--- server/src/dtos/userDto.mjs
+++ server/src/dtos/userDto.mjs
@@ -1,4 +1,4 @@
-const PUBLIC_FIELDS = ['userUuid', 'username', 'displayName', 'createdAt']
+const PUBLIC_FIELDS = ['userUuid', 'username', 'createdAt']
 const PRIVATE_FIELDS = ['updatedAt']
 
 export default class UserDto {
