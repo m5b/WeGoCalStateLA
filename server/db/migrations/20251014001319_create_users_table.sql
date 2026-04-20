4d022a9e2a85306863a83c1e88488aee6f804ee7
Just-ARandomGuy <baohang873@gmail.com>
2026-04-19 19:26:06 -0700

    remove unnecessary field due to implmentation of idp

--- server/db/migrations/20251014001319_create_users_table.sql
+++ server/db/migrations/20251014001319_create_users_table.sql
@@ -2,14 +2,10 @@
 CREATE TABLE users (
     user_id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     user_uuid BINARY(16) NOT NULL UNIQUE,
-    password_hash varchar(255),
-    email_hash BINARY(32) UNIQUE,
     username varchar(100) UNIQUE,
-    display_name varchar(100),
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP,
-    account_status ENUM('pending_verification', 'active', 'deactivated') DEFAULT 'pending_verification'
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP
 );
 
 -- migrate:down
