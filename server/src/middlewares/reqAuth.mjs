d294ae6caefcb53cb740016dabf3d0ba3bac49cc
Just-ARandomGuy <baohang873@gmail.com>
2026-04-19 19:22:20 -0700

    implment new middleware for user authorization Just-in-Time (JIT) provisioning

---
+++ server/src/middlewares/reqAuth.mjs
@@ -0,0 +1,45 @@
+import { jwtVerify } from 'jose'
+import { keycloakConfig} from '../config/authConfig.mjs'
+import { UnauthorizedError } from '../errors/unauthorizedError.mjs'
+import { keycloakJWK } from '../lib/jose.mjs'
+
+export async function reqAuth(userService){
+    return new async function(req, res, next){
+        const accessToken = req.get('X-access-token')
+        if (!accessToken) {
+            console.log("hello")
+            throw new UnauthorizedError({
+                "auth": "Not authorized"
+            }, "You are not authorized, please try again")
+        }
+        try{
+            const { payload, protectedHeader } = await jwtVerify(accessToken, keycloakJWK, {
+                issuer: keycloakConfig.issuer,
+                audience: keycloakConfig.audience
+            });
+            req.accessToken = {
+                userUuid: payload.sub,
+                createdAt: payload.createdTimestamp,
+                username: payload.preferred_username,
+                roles: [
+                    ...(payload.realm_access?.roles ?? []),
+                    ...(payload.resource_access?.['wego-customer']?.roles ?? []),
+                ],
+            }
+
+        }
+        catch (err){
+            throw new UnauthorizedError({
+                "auth": "Not authorized"
+            }, "You are not authorized, please try again")
+        }
+        //check provision
+        let user = await userService.getByUserUuid(req.accessToken.userUuid)
+        if(user == null){
+            const{userUuid, createdAt, username} = req.accessToken;
+            user = await userService.createUser({userUuid, username, createdAt});
+        }
+        req.user = user;
+        return next()
+    }
+}
