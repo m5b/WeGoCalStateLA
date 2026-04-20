14bd4ba70c84910f60a32fb558dd7943a364c55d
Just-ARandomGuy <baohang873@gmail.com>
2026-04-19 23:09:05 -0700

    use dto to render [deleted]

--- server/src/dtos/threadDto.mjs
+++ server/src/dtos/threadDto.mjs
@@ -3,7 +3,12 @@ const PUBLIC_FIELDS = ['threadUuid','userUuid', 'content','title', 'username', '
 export default class ThreadDto {
     constructor(thread) {
         for (let i = 0; i < DEFINE_PUBLIC_FIELDS.length; i++) {
-            this[DEFINE_PUBLIC_FIELDS[i]] = thread[PUBLIC_FIELDS[i]]
+            if(thread[PUBLIC_FIELDS[i] === null]){
+                this[DEFINE_PUBLIC_FIELDS[i]] =  "[Deleted]"
+            }
+            else{
+                this[DEFINE_PUBLIC_FIELDS[i]] = thread[PUBLIC_FIELDS[i]]
+            }
         }
     }
 }
