d60abee402decff0a03e16af629aac88e041c2c3
Just-ARandomGuy <baohang873@gmail.com>
2026-04-19 20:38:39 -0700

    rework on get self api to adopt the implementation of keycloak and oauth2 proxy

--- server/src/routes/userRoutes.mjs
+++ server/src/routes/userRoutes.mjs
@@ -1,31 +1,30 @@
 import { Router } from 'express'
-import {requireJWTAuth} from '../middlewares/requireCookie.mjs'
 import {
-    userIdSchema,
-    userSchema,
     usernameSchema,
 } from '../validators/userValidators.mjs'
 import UserDto from '../dtos/userDto.mjs'
 import { jsend } from '../util/jSend.mjs'
+import { reqAuth } from '../middlewares/reqAuth.mjs'
 
 export function createUserRouter(userService){
 
     const router = Router()
 
-    router.get('/me', requireJWTAuth, async (req, res) => {
-        const user = await userService.getByUuid(req.userUuid)
-        const userDisplay = new UserDto(user, { scope: 'private' })
-        res.status(200).json(jsend.success({ user: userDisplay }))
+    router.get('/me', reqAuth(userService),  async (req, res) => {
+        res.status(200).json(jsend.success({ user: req.user}))
     })
 
-    router.patch('/me', requireJWTAuth, async (req, res) => {
+    // not enabled due to no field can be updated as for now
+    /*
+    router.patch('/me',  async (req, res) => {
         const payload = userSchema.parse(req.body)
         const user = await userService.patchByUserUuid(req.userUuid, payload)
         const userDisplay = new UserDto(user, { scope: 'private' })
         res.status(200).send(jsend.success({ user: userDisplay }))
     })
+     */
 
-    router.delete('/me', requireJWTAuth, async (req, res) => {
+    router.delete('/me', async (req, res) => {
         const result = await userService.deleteByUserUuid(req.userUuid)
         res.status(200).send(jsend.success(null))
     })
