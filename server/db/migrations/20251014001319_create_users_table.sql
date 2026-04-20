247942c5873a6495984fdef65a6442a0757c3c61
Just-ARandomGuy <baohang873@gmail.com>
2026-04-19 19:41:28 -0700

    add unique on username field

--- server/db/migrations/20251014001319_create_users_table.sql
+++ server/db/migrations/20251014001319_create_users_table.sql
@@ -2,7 +2,7 @@
 CREATE TABLE users (
     user_id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     user_uuid BINARY(16) NOT NULL UNIQUE,
-    username varchar(100) UNIQUE,
+    username varchar(100) Not NULL UNIQUE,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
     deleted_at TIMESTAMP
