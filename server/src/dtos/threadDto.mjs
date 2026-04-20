8253374a7e2061fa2a05e55eda74fef36c02dbee
Just-ARandomGuy <baohang873@gmail.com>
2026-04-19 23:48:13 -0700

    fix the bug the '[Deleted]' not appear

--- server/src/dtos/threadDto.mjs
+++ server/src/dtos/threadDto.mjs
@@ -3,7 +3,7 @@ const PUBLIC_FIELDS = ['threadUuid','userUuid', 'content','title', 'username', '
 export default class ThreadDto {
     constructor(thread) {
         for (let i = 0; i < DEFINE_PUBLIC_FIELDS.length; i++) {
-            if(thread[PUBLIC_FIELDS[i] === null]){
+            if(thread[PUBLIC_FIELDS[i]] === null){
                 this[DEFINE_PUBLIC_FIELDS[i]] =  "[Deleted]"
             }
             else{
