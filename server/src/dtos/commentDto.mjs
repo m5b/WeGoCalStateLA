c42f5ba1d58f25f4665ff3db0440016dea119208
Just-ARandomGuy <baohang873@gmail.com>
2026-04-20 19:36:34 -0700

    update the comment dto to fit current table

--- server/src/dtos/commentDto.mjs
+++ server/src/dtos/commentDto.mjs
@@ -1,9 +1,14 @@
-const DEFINE_PUBLIC_FIELDS = ['commentUuid', 'parentCommentUuid', 'content', 'username', "userUuid", "threadUuid", "createdAt", "updatedAt", "status", ]
-const PUBLIC_FIELDS = ['commentUuid', 'parentCommentUuid', 'content', 'username', "userUuid", "threadUuid", "createdAt", "updatedAt", "status", ]
+const DEFINE_PUBLIC_FIELDS = ['commentUuid', 'content', 'username', "userUuid", "threadUuid", "createdAt", "updatedAt", "status", ]
+const PUBLIC_FIELDS = ['commentUuid', 'content', 'username', "userUuid", "threadUuid", "createdAt", "updatedAt", "status", ]
 export default class CommentDto {
     constructor(comment){
             for (let i = 0; i < DEFINE_PUBLIC_FIELDS.length; i++) {
-                this[DEFINE_PUBLIC_FIELDS[i]] = comment[PUBLIC_FIELDS[i]]
+                if (comment[PUBLIC_FIELDS[i]] === null) {
+                    this[DEFINE_PUBLIC_FIELDS[i]] = '[Deleted]'
+                } else {
+                    this[DEFINE_PUBLIC_FIELDS[i]] = comment[PUBLIC_FIELDS[i]]
+                }
+                this["parentCommentUuid"] = comment["parentCommentUuid"]
             }
     }
 }
